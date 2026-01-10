6b4ffb505c5e290c856ed5a63d34dc900f3c7344
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-01-10 19:16:14 +0900

    feat: add variable out to reduce redundancy of recipes

--- src/models/recipe.rs
+++ src/models/recipe.rs
@@ -7,6 +7,7 @@ pub struct Recipe {
     pub id: String,
     pub by: Vec<String>,
     pub time: u32,
+    out: Option<u32>,
     #[serde(default)]
     pub inputs: HashMap<String, u32>,
     #[serde(default)]
@@ -15,6 +16,10 @@ pub struct Recipe {
 
 impl Recipe {
     pub fn normalize(&mut self) {
+        if let Some(count) = self.out {
+            self.outputs.insert(self.id.clone(), count);
+        }
+
         if let Some(count) = self.outputs.remove(SELF_REFERENCE_KEYWORD) {
             self.outputs.insert(self.id.clone(), count);
         }
